7cb72659fb879cd4a24e64d1e37eba62fb73c7cd
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-07-31 12:25:03 +0530

    update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   env: {
-    "es2015": true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
   },
   rules: {
-    "semi": "warn",
+    semi: "warn",
     "array-callback-return": ["error", { checkForEach: true }],
     "no-cond-assign": "error",
     "no-dupe-keys": "error",
@@ -12,24 +15,11 @@ module.exports = {
     "no-this-before-super": "error",
     "use-isnan": "error",
     "valid-typeof": "error",
-    "camelcase": "warn",
-    "complexity": ["warn", 10],
+    camelcase: "warn",
+    complexity: ["warn", 10],
     "default-param-last": "error",
     "dot-notation": "warn",
-    "eqeqeq": "warn",
-    "id-denylist": [
-      "error",
-      "data",
-      "err",
-      "e",
-      "cb",
-      "callback",
-      "string",
-      "obj",
-      "arr",
-      "string",
-      "str"
-    ],
+    eqeqeq: "warn",
     "max-depth": ["warn", 3],
     "max-statements": "warn",
     "new-cap": "error",
@@ -43,9 +33,8 @@ module.exports = {
     "no-useless-escape": "warn",
     "prefer-const": "warn",
     "comma-spacing": ["warn", { before: false, after: true }],
-    "indent": ["error", "tab"],
-    "key-spacing": ["error", { "beforeColon": true }],
+    "key-spacing": ["error", { beforeColon: true }],
     "max-len": "error",
-    "no-multiple-empty-lines": "error"
+    "no-multiple-empty-lines": "error",
   },
 };
